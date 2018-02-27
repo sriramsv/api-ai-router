4cf5bb1f1783089d21a92bb324e791a94f6a3eb4
sriramvenkat <sriramsv007@gmail.com>
2018-02-27 00:16:08 -0800

    Initial

--- apiai_router/__init__.py
+++ apiai_router/__init__.py
@@ -2,14 +2,12 @@ import yaml
 import flask
 import sys,os
 from utils.bots import BotEnsemble,Bot
-from utils.join import  Join
-from utils.result import json2obj
 import apiai
 import json,yaml
 from flask import Flask,request,jsonify,Response,Session
 
 botensemble=BotEnsemble()
-joinObj=Join()
+
 app = Flask(__name__)
 
 CONFIG="config.yaml"
@@ -24,21 +22,6 @@ def read_config():
         bot=Bot(b['name'].lower(),b['client_access_token'])
         botensemble.addBot(bot)
 
-    if "join" in cfg:
-        joind=cfg['join']
-        try:
-            api_key=joind['api_key']
-        except:
-            sys.exit("Cant find join api_key")
-        joinObj.api_key=api_key
-
-        for d in joind['devices']:
-            try:
-                joinObj.addDevice(d['name'],d['device_id'])
-            except KeyError:
-                sys.exit("Join device name or ID missing")
-
-
 
 def api_ai_request(bot,query):
     token=bot.get_token()
@@ -47,8 +30,6 @@ def api_ai_request(bot,query):
     request.query = query
     response = request.getresponse()
     r=response.read()
-    rbj=json2obj(r)
-    joinObj.sendMessage("phone",rbj)
     return r
 
 
@@ -62,8 +43,11 @@ def router(botname):
         query=request.args.get('query','')
     elif request.method=='POST':
         if not request.is_json:
-            return Response(500,"Json expected")
+            return Response(400,"Invalid Request")
         query=request.get_json()["query"]
+
+    if not query:
+        return Response(400,"Invalid request")
     if not botensemble.isBot(botname):
         return Response(404,"Bot Not found")
     bot=botensemble.getBot(botname)
